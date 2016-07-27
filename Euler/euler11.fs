7eb5356f68662ef8d4524da662b45c1bb7fea212
chlorophilistine <c@philistine.org>
2016-07-27 01:33:33 +0100

    a go at 12

--- Euler/euler11.fs
+++ Euler/euler11.fs
@@ -1,4 +1,10 @@
+open System
 open System.IO
 
 let data =
-    File.ReadAllText(@"Euler/euler11.txt")
+    File.ReadAllLines(@"euler11.txt")
+    |> Seq.map (fun line -> line.Split(' ') |> Seq.map int32 |> Seq.toArray)
+    |> Seq.toArray
+
+let Data2d =
+    Array2D.init 20 20 (fun i j -> data.[i].[j])
---
+++ Euler/euler12.fs
@@ -0,0 +1,15 @@
+open System
+
+let factorsOf (x: int64) =
+    let upperBound = int64(Math.Sqrt(double(x)))
+    [1L..upperBound]
+    |> Seq.where (fun i -> x % i = 0L)
+    |> Seq.collect (fun n -> [n; x/n])
+
+let triangularNumbers =
+    Seq.initInfinite (fun index -> seq {for i in 1 .. (index + 1) -> i} |> Seq.sum)
+
+let answer12 =
+    triangularNumbers
+    |> Seq.where (fun tnum -> (int64(tnum) |> factorsOf |> Seq.length) > 500)
+    |> Seq.head
