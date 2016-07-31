b703d84f4a57d5833d583131c84c889df389aed5
chlorophilistine <c@philistine.org>
2016-07-31 15:53:38 +0100

    22 answered and learned how to use map2 (or zip)

---
+++ Euler/euler22.fs
@@ -0,0 +1,27 @@
+open System
+open System.IO
+
+let scoreString name =
+    name |> Seq.map (fun c -> int(c) - 64)
+    |> Seq.sum
+
+let test = scoreString ("abcdefghijklmnopqrstuvwxyz".ToUpper())
+
+let names =
+    File.ReadAllLines("p022_names.txt")
+    |> Seq.map (fun s -> s.Replace("\"", ""))
+    |> Seq.collect (fun s -> s.Trim().Split(','))
+    |> Seq.map (fun s -> s.ToUpper())
+    |> Seq.sort
+
+let scores =
+    names |> Seq.map scoreString
+
+let position =
+    seq{1..Seq.length scores}
+
+let answer22 =
+    Seq.map2 (fun pos score -> pos * score)  scores position
+    |> Seq.sum
+
+
