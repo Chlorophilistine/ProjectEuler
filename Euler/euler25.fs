8059b4f5810cb3646ca5f987ef319d8e827deda4
chlorophilistine <c@philistine.org>
2016-08-05 17:00:53 +0100

    Use Seq.findIndex rather than map2 with index sequence

--- Euler/euler25.fs
+++ Euler/euler25.fs
@@ -3,12 +3,6 @@ open System
 let fibonacci =
     Seq.unfold (fun(current, next) -> Some(current, (next, current + next))) (1I, 1I)
 
-let countFrom1 =
-    Seq.initInfinite (fun index -> index + 1)
-
 let answer25 =
-    fibonacci 
-    |> Seq.map2 (fun index fib -> (index, fib)) countFrom1
-    |> Seq.where (fun (index, fib) -> fib.ToString() |> Seq.length = 1000)
-    |> Seq.head
-    |> fst
+    (fibonacci 
+    |> Seq.findIndex (fun fib -> fib.ToString() |> Seq.length = 1000) ) + 1
